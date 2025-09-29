2a5515c5579cbda9d5a444e1e548cb3e6720ab95
Mehdi Mehdikhani <hello@mehdik.dev>
2025-09-29 18:14:50 +0200

    docs: fix dot product documentation referring to cross product

--- src/include/refx/geometry/vector.h
+++ src/include/refx/geometry/vector.h
@@ -878,8 +878,8 @@ Vector3D<Frame1, T> cross(const Vector3D<Frame1, T>& a, const Vector3D<Frame2, T
  *
  * @tparam Frame The shared reference frame of the vectors.
  * @tparam T The scalar type.
- * @param a The left-hand side vector in the cross product (`a x b`).
- * @param b The right-hand side vector in the cross product (`a x b`).
+ * @param a The left-hand side vector in the dot product (`a · b`).
+ * @param b The right-hand side vector in the dot product (`a · b`).
  * @return a scalar representing the dot product `a . b`.
  * @warning This operation is only physically meaningful for vectors in Cartesian
  * reference frames (e.g., `NED`, `FRD`, `ECEF`). Using it on non-Cartesian types
